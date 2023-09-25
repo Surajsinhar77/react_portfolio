d182f7cbeacf5a4a6e792b76dbae960f41b1c6e1
Suraj Kumar <surajsinhaking77@gmail.com>
2023-09-26 02:08:16 +0530

    done few change

---
+++ src/components/Dashboard/Dashboard.jsx
@@ -0,0 +1,11 @@
+
+export default function dashboard(){
+    // const [userName, setUserName] = useState("default");
+    return(
+        <>  
+            {/* <AppContext.Provider value={{userName, setUserName}}> */}
+                <LoginPage/>
+            {/* </AppContext.Provider> */}
+        </>
+    )
+};
